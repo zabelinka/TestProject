e183a088a59fa481e7febbb5905ffb890f73f657
Tatyana Zabelina <zabelina-tanya@mail.ru>
2016-02-21 15:57:47 +0300

    Initial commit

---
+++ gui.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-02-21T15:07:04
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = gui
+TEMPLATE = app
+
+INCLUDEPATH += /usr/local/include/opencv
+
+LIBS += -L/usr/local/lib -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_videoio
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <QDebug>
+#include <opencv2/opencv.hpp>
+#include <opencv2/videoio.hpp>
+#include <stdio.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    cv::VideoCapture cap(0);
+
+    cv::Mat frame;
+    cv::namedWindow("video", 1);
+    while ( cap.isOpened() )
+    {
+        cap >> frame;
+        if(frame.empty()) break;
+
+        imshow("video", frame);
+        if(cv::waitKey(30) >= 0) break;
+    }
+
+    return a.exec();
+}
