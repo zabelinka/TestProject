f73ac50795a27cb18ad25a46dc681456587e21bb
Tatyana Zabelina <zabelina-tanya@mail.ru>
2016-02-22 11:37:07 +0300

    simple video player

--- main.cpp
+++ main.cpp
@@ -5,20 +5,27 @@
 #include <stdio.h>
 #include <iostream>
 
+using namespace cv;
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    cv::VideoCapture cap("drop.avi");
+    VideoCapture cap("drop.avi");
 
-    cv::Mat frame;
-    cv::namedWindow("video", 1);
+    if(!cap.isOpened()){    // check if we succeeded
+        std::cout << "Cannot open the video file" << std::endl;
+        return -1;
+    }
+
+    Mat frame;
+    namedWindow("video", 1);
     while ( cap.isOpened() )
     {
         cap >> frame;
         if(frame.empty()) break;
 
         imshow("video", frame);
-        if(cv::waitKey(30) >= 0) break;
+        if(waitKey(30) >= 0) break;
     }
 
     return a.exec();
